f6ecdcb6786a16dfab9d1b69713a3a9a7828e212
Aika <aika-kk-99@mail.ru>
2021-03-14 07:37:31 -0700

    first commit

---
+++ git.py
@@ -0,0 +1,15 @@
+git pull origin master
+git remote add name_url
+git pull origin master
+git status
+git add
+git add name_file
+git add.
+git commit
+git commit -m "comment"
+git branch
+git branch name_branch
+git checkout name_branch
+git push name_branch
+git push origin master
+git reset file_name
